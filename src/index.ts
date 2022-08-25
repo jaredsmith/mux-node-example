71609ebed0a67c5e7ababdc8b4df3c1c4ec213b2
Jared Smith <jsmith@mux.com>
2022-08-25 14:49:03 -0400

    More fixes to style, etc.
    Signed-off-by: Jared Smith <jsmith@mux.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import Mux from '@mux/mux-node';
-import dotenv from 'dotenv';
+import Mux from "@mux/mux-node";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,10 +8,12 @@ const { Video, Data } = new Mux();
 // assume process.env.MUX_TOKEN_ID and process.env.MUX_TOKEN_SECRET contain your credentials
 const muxClient = new Mux(); // Success!
 
-const assets = Video.Assets.get('5CY00Lk00omn5zaf3E6B6UxFR9Gi02CiBKBtptFT02Owvio')
-  .then(results => {
+const assets = Video.Assets.get(
+  "5CY00Lk00omn5zaf3E6B6UxFR9Gi02CiBKBtptFT02Owvio"
+)
+  .then((results) => {
     console.log(results.recording_times);
   })
-  .catch(error => {
+  .catch((error) => {
     console.warn(error);
   });
